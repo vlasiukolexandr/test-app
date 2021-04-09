cc2b0b5778f52d7f3ec2805a57926dc35025ff62
olexandr_v <olexandr.v@scopicsoftware.com>
2021-04-10 00:00:06 +0300

    formatted json

--- be-side/controllers/simpleController/simpleController.ts
+++ be-side/controllers/simpleController/simpleController.ts
@@ -1,6 +1,8 @@
 import { Request, NextFunction, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 
+import { Database } from '../../database';
+
 export class SimpleController {
   /**
    * Say "Hello world!"
@@ -8,11 +10,28 @@ export class SimpleController {
    * @param res Response
    * @param next
    */
-  public static async hello(req: Request, res: Response, next: NextFunction) {
+  public hello(req: Request, res: Response, next: NextFunction) {
     // Clearer response for me
     // res.status(StatusCodes.OK).send({ status: StatusCodes.OK, data: { message: 'Hello world!' }});
 
     // Response like in specs
     res.status(StatusCodes.OK).send('Hello world!');
+  }
+
+  /**
+   * Format DB JSON
+   * @param req Request
+   * @param res Response
+   * @param next
+   */
+  public jsonFormat(req: Request, res: Response, next: NextFunction) {
+    Database.connection.getConnection().then(db => {
+      db.ref().on('value', (snap: any) => {
+        const data = snap.val();
+        
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(data, null, 2));
+      });
+    });
   }
 }
--- be-side/routes/simpleRoute.ts
+++ be-side/routes/simpleRoute.ts
@@ -3,7 +3,10 @@ import { SimpleController } from '../controllers';
 
 export class SimpleRoute extends BaseRoute {
   public init() {
-    this.get('/hello', SimpleController.hello);
+    const simpleController = new SimpleController();
+
+    this.get('/hello', simpleController.hello);
+    this.get('/formatted_json', simpleController.jsonFormat);
     return this.router;
   }
 }
